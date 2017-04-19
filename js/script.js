42f54789b5b98564ed719f9d8673db68be9baedb
CoDeRivera <CoDeRivera@users.noreply.github.com>
2017-04-19 09:57:27 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$('.default').hide();
+$('#left-door').hide();
+$('#right-door').hide();
+                  
+$('#navbar li').click(function(){
+	$('#left-door').animate({width:'toggle'}).delay(450).animate({width:'toggle'});
+	$('#right-door').animate({width:'toggle'}).delay(450).animate({width:'toggle'});
+	$('.default').delay(200).fadeOut();
+	$('#' + this.id +'-content').fadeToggle();
+});
+
+
+
+$('#clear').click (function(){
+	$('.default').hide();
+});	
+
+$('#activate').click(function(){
+	$('#left-door').animate({width:'toggle'});
+	$('#right-door').animate({width:'toggle'})
+});
+
